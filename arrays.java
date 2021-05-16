fa822ae86c82303fce74122ff76240fb1cee22d2
devendra <you@example.com>
2021-05-16 12:42:32 +0530

    recurssion

--- arrays.java
+++ arrays.java
@@ -1,6 +1,16 @@
 public class arrays {
     public static void main(String[] args) {
-        String[] cars = {"1", "2", "3", "4"};
-        System.out.println(cars[0]);
+        String[] nums = { "1", "2", "3", "4" };
+        System.out.println(nums[0]);
+        nums[0] = "2";
+        System.out.println(nums[0]);
+        System.out.println(nums.length);
+        for (String i : nums) {
+            System.out.println(i);
+        }
+
+        int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+        int x = myNumbers[1][1];
+        System.out.println(x);
     }
 }
